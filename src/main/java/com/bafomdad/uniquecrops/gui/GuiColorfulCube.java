3c5a74fdffcd1869d860a9d5c16a67323f0687d9
bafomdad <cheesecake@operamail.com>
2021-07-25 11:17:51 -0700

    bandaid fix for rubik's cube gui. I will come back to this at some point

--- src/main/java/com/bafomdad/uniquecrops/gui/GuiColorfulCube.java
+++ src/main/java/com/bafomdad/uniquecrops/gui/GuiColorfulCube.java
@@ -7,10 +7,13 @@ import com.bafomdad.uniquecrops.network.PacketColorfulCube;
 import com.bafomdad.uniquecrops.network.UCPacketHandler;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.DialogTexts;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.item.ItemStack;
@@ -52,6 +55,7 @@ public class GuiColorfulCube extends Screen {
     @Override
     public void render(MatrixStack ms, int mouseX, int mouseY, float partialTicks) {
 
+        //TODO: real fix
         ms.push();
         RenderSystem.enableBlend();
         RenderSystem.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
@@ -59,6 +63,7 @@ public class GuiColorfulCube extends Screen {
         float zLevel = minecraft.getItemRenderer().zLevel;
         ms.translate(0, 0, zLevel - 0.25);
         ms.scale(0.25F, 0.25F, 0.25F);
+//        ms.translate(-0.125, -0.125, 0);
         Vector3i vec3 = this.getRotationVec();
         Vector3f vecf = new Vector3f(vec3.getX(), vec3.getY(), vec3.getZ());
         boolean north = vec3.equals(Direction.NORTH.getDirectionVec());
@@ -86,9 +91,10 @@ public class GuiColorfulCube extends Screen {
                 ms.rotate(Vector3f.YP.rotationDegrees(180F));
         }
         ItemStack cubeStack = new ItemStack(UCItems.RUBIKS_CUBE.get());
-        IRenderTypeBuffer.Impl renderBuffer = minecraft.getRenderTypeBuffers().getBufferSource();
+        IRenderTypeBuffer.Impl renderBuffer = minecraft.getRenderTypeBuffers().getCrumblingBufferSource();
         itemRenderer.renderItem(cubeStack, ItemCameraTransforms.TransformType.FIXED, 0xF000F0, OverlayTexture.NO_OVERLAY, ms, renderBuffer);
-        renderBuffer.finish();
+//        IBakedModel model = itemRenderer.getItemModelWithOverrides(cubeStack, null, null);
+//        itemRenderer.renderModel(model, cubeStack, 15728880, OverlayTexture.NO_OVERLAY, ms, renderBuffer.getBuffer(RenderType.getCutout()));
         RenderSystem.disableBlend();
         ms.pop();
         super.render(ms, mouseX, mouseY, partialTicks);
@@ -169,7 +175,7 @@ public class GuiColorfulCube extends Screen {
 
     private ItemStack getCube() {
 
-        ItemStack stack = minecraft.player.getHeldItemMainhand();
+        ItemStack stack = Minecraft.getInstance().player.getHeldItemMainhand();
         if (stack.getItem() == UCItems.RUBIKS_CUBE.get())
             return stack;
 
